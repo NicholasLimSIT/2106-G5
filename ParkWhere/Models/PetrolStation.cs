8b79283028a719b850cda5ced09eea61fe7397e2
Wei Jian <weijian.vip@gmail.com>
2016-03-19 23:42:36 +0800

    Updated Models

--- ParkWhere/Models/PetrolStation.cs
+++ ParkWhere/Models/PetrolStation.cs
@@ -6,12 +6,12 @@
 //     Manual changes to this file will be overwritten if the code is regenerated.
 // </auto-generated>
 //------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ParkWhere.Models
 {
-    using System;
-    using System.Collections.Generic;
-    
     public partial class PetrolStation
     {
         public int Id { get; set; }
