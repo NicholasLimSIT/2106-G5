319f83453b4dd6a2b5472eef14bf47febf3f40bb
Wei Jian <weijian.vip@gmail.com>
2016-03-28 22:07:21 +0800

    Added Top Five Bookmarked Carparks Table

--- ParkWhere/Controllers/CarparksController.cs
+++ ParkWhere/Controllers/CarparksController.cs
@@ -13,15 +13,19 @@ namespace ParkWhere.Controllers
 {
     public class CarparksController : GeneralController<Carpark>
     {
+        //To store top five bookmarked carparks
+        IEnumerable<Carpark> TopFiveCarparks;
+
         public CarparksController() {
             dataGateway = new CarparkGateway();
+            TopFiveCarparks = ((CarparkGateway)dataGateway).GetTopFiveBookmarks();
         }
 
-        public override ActionResult Index(int? id)
-        {
-            ViewBag.List = (((CarparkGateway)dataGateway).GetAllCarparks());
-            return View();
-        }
+        //public override ActionResult Index(int? id)
+        //{
+        //    ViewBag.List = (((CarparkGateway)dataGateway).GetAllCarparks());
+        //    return View();
+        //}
 
         [HttpPost]
         public ActionResult Index(string value1, string value2)
@@ -29,12 +33,12 @@ namespace ParkWhere.Controllers
             if (Session["searchResult"] == null)
             {
                 ViewBag.List = ((CarparkGateway)dataGateway).FilterByType(value1, value2);
-                return View();
+                return View(TopFiveCarparks);
             }
             else {
                 string searchResult = Session["searchResult"].ToString();
                 ViewBag.List = ((CarparkGateway)dataGateway).FilterAddressByType(searchResult, value1, value2);
-                return View();
+                return View(TopFiveCarparks);
             }
         }
 
@@ -43,7 +47,7 @@ namespace ParkWhere.Controllers
         {
             Session["searchResult"] = addResults;
             ViewBag.List = ((CarparkGateway)dataGateway).searchCarpark(addResults);
-            return View();
+            return View(TopFiveCarparks);
         }
 
     }
